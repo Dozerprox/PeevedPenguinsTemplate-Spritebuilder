6660225acab587d57cbc2f9e9867bebae54e7199
Viktor Vaughn <sbsteven@andrew.cmu.edu>
2015-02-03 01:30:39 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Dozer on 2/3/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
